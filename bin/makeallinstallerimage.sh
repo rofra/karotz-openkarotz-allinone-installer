bd4e1bd1480513d36043ef1ee1f7a8debf9386ff
Rodolphe Franceschi <rodolphe.franceschi@gmail.com>
2014-10-22 13:23:59 +0200

    Generator upgraded

---
+++ bin/makeallinstallerimage.sh
@@ -0,0 +1,48 @@
+#/bin/bash
+
+CURRDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOTDIR=$CURRDIR/../
+cd $ROOTDIR
+
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+if [ ! -d "${ROOTDIR}firmware" ]; then
+  echo "firmware not found"
+  exit 1
+fi
+
+if [ ! -d "${ROOTDIR}firmware" ]; then
+  echo "websources not found"
+  exit 1
+fi
+
+echo "Generating Firmware"
+cd firmware
+./bin/makeroms.sh
+cd ${ROOTDIR}
+
+
+if [ ! -f ${ROOTDIR}firmware/rootfs.img.gz ] || [ ! -f ${ROOTDIR}firmware/rootfs.img.gz.md5 ] ; then
+  echo "ERROR, firmware not generated"
+  rm -f ${ROOTDIR}firmware/rootfs.img.gz ${ROOTDIR}firmware/rootfs.img.gz.md5
+  exit 1
+fi
+
+echo "Compiling Files in a new image directory"
+rm -fr ${ROOTDIR}generatedimage/
+mkdir ${ROOTDIR}generatedimage/
+cp -f ${ROOTDIR}ressources/autorun  ${ROOTDIR}ressources/autorun.sig ${ROOTDIR}ressources/tools2.tar  ${ROOTDIR}generatedimage/ 
+cp -fR ${ROOTDIR}ressources/sound ${ROOTDIR}generatedimage/ 
+mv -f ${ROOTDIR}firmware/rootfs.img.gz ${ROOTDIR}firmware/rootfs.img.gz.md5 ${ROOTDIR}generatedimage/
+cp -fR ${ROOTDIR}websources/ ${ROOTDIR}generatedimage/openkarotz/
+
+echo "Managing rights problems"
+chmod -R 755 ${ROOTDIR}generatedimage/
+
+echo "OK! Your installer is generated in generatedimage directory"
+echo "End of the generation"
+
+
---
+++ ressources/autorun.sig
@@ -0,0 +1,16 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA1
+
+12.07.19.00
+http://update.karotz.com/eabi/rootfs/rootfs-12.07.19.00.tar.gz ecc92baed07352d670be4d8c9beeb5d0
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1.4.11 (GNU/Linux)
+
+iQEcBAEBAgAGBQJQB9NiAAoJEDV3C7tYF9eUXG0H/jkq/UZmSbOClPiRu+XhtTfn
+4kR4lRdlEU9CFuiTzkWrrUJ7g/klvyEm8ecAH7UZGkeq/xfhjDeWJZqZnd6SZHyx
+By39r54cxIxiI8w3mxdloFF1NwqywvRlx4cExwpi/ku5MpLwy1nXmMTHGSuSezeh
+/HTugyPmXuu9Nr/Kjv1YxC8wBU5XalQXl0qptt9JVGXJn+oNi7wNNHay8R0WY2xv
+CZRmdCrBtE8+383J7tN6mUzTxYZZsb8tL+OSjLLAICsRLBAUXCS2ZxjnsMOPEvGG
+FmKVP6U9FQ4uZqROGGnvFqERD3wBDL6yqlBu3Mmk15bkpkcSelQw/Ei3FJ/mfU0=
+=eM43
+-----END PGP SIGNATURE-----
